7c23a337b4d0995293c91c0ef3b828317e3b9b68
abramo0 <azerabramo@gmail.com>
2026-06-15 12:13:32 +0200

    Update schema.sql

--- datasets/library/schema.sql
+++ datasets/library/schema.sql
@@ -1,1 +1,26 @@
+CREATE TABLE clients (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    city VARCHAR(50)
+);
 
+CREATE TABLE products (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    price DECIMAL(8,2)
+);
+
+CREATE TABLE orders (
+    id INT PRIMARY KEY,
+    client_id INT,
+    product_id INT,
+    quantity INT,
+    date_order DATE,
+
+    FOREIGN KEY (client_id)
+        REFERENCES clients(id),
+
+    FOREIGN KEY (product_id)
+        REFERENCES products(id)
+);
